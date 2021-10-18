d528eec61cf4a0eb1a0856ece3f0b0268274d33d
Damien Hampton <damien.hampton@joyn.de>
2021-10-18 10:30:04 +0100

    removed extraneous js file

--- test/Greeter.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Greeter_1 = require("../src/Greeter");
-describe('Greeter', function () {
-    it('Should output "Hello World"', function () {
-        expect(Greeter_1.Greeter.greet()).toBe('Hello World');
-    });
-});
